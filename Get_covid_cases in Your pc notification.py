402ef30d15b57da3a401ba57f1bf0fd09ae3718e
Sachin Chauhan <68002548+Sachinbeetu@users.noreply.github.com>
2020-07-30 14:26:10 +0530

    Create Get_covid_cases in Your pc notification.py

---
+++ Get_covid_cases in Your pc notification.py
@@ -0,0 +1,37 @@
+from plyer import notification
+#https://api.covid19india.org/state_district_wise.json
+import requests
+import json
+import time
+url='https://api.covid19india.org/state_district_wise.json'
+data=requests.get(url).text
+def notify(title,message):
+    notification.notify(title=title,app_icon='D:\Projects\Python Projects\PycharmProjects\covid\icon.ico',message=message,timeout=7)
+if __name__ == '__main__':
+    dict = json.loads(data)
+    s = input('Enter State name for data access(Case sensitive):')
+    i = input('Enter District name (Case Sensitive):')
+    if len(s)<1 and len(i)<1:
+        s='Uttar Pradesh'
+        i='Lucknow'
+    state = dict[s]
+    dists = state['districtData']
+    dist = dists[i]
+    new=dist['delta']
+    message=f'''
+    Active Cases:' {dist['active']}
+    Confirmed Cases:' {dist['confirmed']}
+    Deceased Cases:' {dist['deceased']}
+    Recovered Cases: {dist['recovered']}
+    '''
+    newcases =f'''
+    New cases: 
+    Confirmed: {new['confirmed']}
+    Recovered: {new['recovered']}
+    Deceased : {new['deceased']}'''
+    print(message)
+    notify(f'Covid{s,i}', message)
+    time.sleep(7)
+    notify(f'Covid{s, i}', newcases)
+    print(newcases)
+
